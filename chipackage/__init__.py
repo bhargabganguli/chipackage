f2daaa35e1120267637f4bb0f9d44f8dbcacf355
bhargabganguli <89978856+bhargabganguli@users.noreply.github.com>
2022-09-24 21:20:41 +0530

    Update __init__.py

--- chipackage/__init__.py
+++ chipackage/__init__.py
@@ -1,16 +1,2 @@
-import numpy as np
-import pandas as pd
-
-def functionadd(arg1, arg2):
-    """
-        Description of function
-        Args:
-            arg1 (integer): description of arg1
-            arg2 (integer): description of arg2
-        Returns:
-            return1 (type): description of return 
-    """
-    return1= arg1+arg2
-    return(return1)
-
+from hello import sayhello
     
